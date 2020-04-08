e0a57a4fcb8717d800908bc16fad8638b3d80890
Dmitry Grechka <dmitry@grechka.family>
2020-04-08 14:17:36 +0300

    adding missing gatherSEIRParams.R file under SC

---
+++ code/gatherParamsSEIR.R
@@ -0,0 +1,64 @@
+require(RJSONIO)
+args = commandArgs(trailingOnly=TRUE)
+
+inputPattern <- args[1]
+inputPattern <- substr(inputPattern,2,nchar(inputPattern)-1) # trimming out ''
+outFile <- args[2]
+
+asterixInPos = regexpr('\\*',inputPattern)[1]
+print(paste0("asterisk pos is ",asterixInPos))
+inDir <- substr(inputPattern,1,asterixInPos-2)
+filemask <- substr(inputPattern,asterixInPos,nchar(inputPattern))
+
+print(paste0("Looking for the files ",filemask," in ",inDir))
+inFiles <- dir(path=inDir,pattern=paste0(filemask,'$'))
+
+acc <- NULL
+
+start_date <- as.Date(strptime('2020-01-01',format='%Y-%m-%d',tz="GMT"))
+
+for(pars_df_path in inFiles) {
+  print(paste0("Loading ",pars_df_path))
+  inFilePath <- file.path(inDir,pars_df_path)
+  json_data <- fromJSON(inFilePath)
+  key <- substr(pars_df_path,1,nchar(pars_df_path)-5)
+  atPos = regexpr('@',key)[1]
+  if(atPos>=1) {
+    province <- substr(key,1,atPos-1)
+    country <- substr(key,atPos+1,nchar(key))
+  } else {
+    province <- ''
+    country <- key
+  }
+  
+  if(is.null(json_data[["R0"]])) {
+    print("invalid R0")
+    next;
+  }
+  
+  pars_df <- data.frame(do.call("rbind",list(json_data)))
+  
+  zeroThreshold <- 5e-3
+  # Qualicy Control
+  if(pars_df$Beta < zeroThreshold || pars_df$Sigma < zeroThreshold || pars_df$Gamma < zeroThreshold) {
+    print(paste0("Skipping '",province,"' country '",country,"' due to too near zero rate parameters (QC)"))
+    next;
+  }
+  
+  origColsN <- ncol(pars_df)
+  
+  #print(pars_df)
+  pars_df$Province <- province
+  pars_df$Country <- country
+  pars_df$FirstDate <- as.character(start_date+pars_df$FirstDayNum-1)
+  pars_df$PeakInfectedDate <- as.character(start_date+pars_df$PeakInfectedDayNum-1)
+  pars_df$PeakExposedDate <- as.character(start_date+pars_df$PeakExposedDayNum-1)
+  pars_df <- pars_df[,c(origColsN+1,origColsN+2,1:origColsN,(origColsN+3):(origColsN+5))]
+  if(is.null(acc))
+    acc <- pars_df
+  else
+    acc <- rbind(acc,pars_df)
+  print(paste0(pars_df_path," processed"))
+}
+
+write.csv(acc,file=outFile,row.names = F, quote = T)
